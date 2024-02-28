385f2b2d9985042d066f06e03a5d7d49dd107506
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-02-28 12:54:47 +0530

    after host

--- config/mongooseConnect.js
+++ config/mongooseConnect.js
@@ -17,33 +17,18 @@
 
 // module.exports =  connectToMongoDB ;
 
-
-
-
-
-
-
-const mongoose = require('mongoose');
-require('dotenv').config()  //env
-const {mongooseConnectionId,mongoosePassword} = process.env;
-
-const password = encodeURIComponent(mongoosePassword); //password URLencoding
-const connection = `mongodb+srv://sanooj:${password}${mongooseConnectionId}`;
+const mongoose = require("mongoose");
 
 const connectToMongoDB = async () => {
-      await mongoose.connect(connection,{
-      useNewUrlParser:true,
-      useUnifiedTopology:true
-  
-}).then(()=>{
+  await mongoose
+    .connect(process.env.MONGOURL)
+    .then(() => {
       console.log(`Connected to MongoDB Atlas`);
-}).catch((error)=>{
+    })
+    .catch((error) => {
       console.log(error.message);
       console.log("Not Connected");
-})
-
+    });
 };
 
-module.exports = { connectToMongoDB };
-
-
+module.exports = connectToMongoDB ;
--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000;
 
 
 
-const { connectToMongoDB } = require("./config/mongooseConnect.js");
+const connectToMongoDB = require("./config/mongooseConnect.js");
 connectToMongoDB();
 
 
